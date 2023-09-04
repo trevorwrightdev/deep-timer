ab9320082b11c99366a68ecc3c637cf7c96aa6ca
Trevor Wright <trevor.wright.710@gmail.com>
2023-09-04 17:54:48 -0600

    created timer component

---
+++ app/components/Timer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type TimerProps = {}
+
+const Timer: React.FC<TimerProps> = () => {
+    return (
+        <div className='w-80 h-80 bg-gray-800 rounded-md'>
+
+        </div>
+    )
+}
+export default Timer
