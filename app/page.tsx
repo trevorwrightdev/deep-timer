b74c7d10aa5d2d7955a18ef1e027937e431f9166
Trevor Wright <trevor.wright.710@gmail.com>
2023-09-04 19:07:30 -0600

    changed container width from full to screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Timer from '@components/Timer'
 
 export default function Home() {
   return (
-    <div className='pt-40 w-full flex flex-col items-center'>
+    <div className='pt-40 w-screen flex flex-col items-center'>
       <Timer />
     </div>
   )
